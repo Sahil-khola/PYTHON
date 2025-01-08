dd517358c6ea13aa14ddfff5179f474fd515386e
Sahil-khola <blackiayaduvanshia@gmail.com>
2025-01-08 22:56:40 +0530

    PYTHON

---
+++ CHAPTER - 1/Module.py
@@ -0,0 +1,16 @@
+import pyjokes
+
+# print('Printing Jokes...')
+
+# This print a random Jokes
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+there are two types modules in python
+1 built in modules
+2 user defined modules
+
+print(72-2)
+print(34+4-34*23)
+"""
