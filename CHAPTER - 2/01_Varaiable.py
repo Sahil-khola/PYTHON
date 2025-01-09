63ee1ee96f47e983e6e56c9955aa083f4afb9ec1
Sahil-khola <blackiayaduvanshia@gmail.com>
2025-01-09 21:53:39 +0530

    PYTHON

---
+++ CHAPTER - 2/01_Varaiable.py
@@ -0,0 +1,4 @@
+a = 1
+b = 2
+name = 'Sahil'
+print(a+b)
---
+++ CHAPTER - 2/03_Rules_Variable.py
@@ -0,0 +1,19 @@
+a = 23
+
+aaa= 222
+
+Sahil = 16
+
+khola = 32
+
+_sahil = 34
+
+# @Sahil = 54
+
+
+# Rules for defining variables
+# 1. Variable name should not start with a number
+# 2. Variable name can start with a letter or underscore
+# 3. Variable name should not contain any special character
+# 4. Variable name should not contain any space
+# 5. Variable name should not be a python keyword
